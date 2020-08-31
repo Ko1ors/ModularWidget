dec97d7f4995fea3ef56e0cb116c1e1d9c0066cb
Ko1ors <kolorsnik181200@gmail.com>
2020-08-31 16:37:19 +0300

    Settings added, GUI update

---
+++ EthWidget/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EthWidget
+{
+    public static class Settings
+    {
+        public static string EthApiKey; // Etherscan.io api key
+        public static string EthWallet;
+        public static int updateTime;
+
+        public static void Save()
+        {
+
+        }
+
+        public static void Load()
+        {
+
+        }
+    }
+}
