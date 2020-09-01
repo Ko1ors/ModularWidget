0302d7e329e526e1b31b3213f3d57ac7e84361c0
Ko1ors <kolorsnik181200@gmail.com>
2020-09-01 14:19:49 +0300

    Data classes added

---
+++ EthWidget/Data/BlockRewardResult.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EthWidget.Data
+{
+    class BlockRewardResult
+    {
+        [JsonProperty("blockNumber")]
+        public string BlockNumber;
+
+        [JsonProperty("timeStamp")]
+        public string TimeStamp;
+
+        [JsonProperty("blockMiner")]
+        public string BlockMiner;
+
+        [JsonProperty("blockReward")]
+        public string BlockReward;
+
+        [JsonProperty("uncles")]
+        public List<object> Uncles;
+
+        [JsonProperty("uncleInclusionReward")]
+        public string UncleInclusionReward;
+    }
+}
---
+++ EthWidget/Data/EthGasPrice.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EthWidget.Data
+{
+    class EthGasPrice : Root
+    {
+        [JsonProperty("result")]
+        public EthGasPriceResult Result;
+    }
+}
