f6012c8e5b5dfabda2a748feb72fc03016e3e38d
Ko1ors <kolorsnik181200@gmail.com>
2020-09-10 19:05:08 +0300

    ETC module start update

---
+++ ETCModule/ETCLoadModule.cs
@@ -0,0 +1,33 @@
+﻿using ModularWidget;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETCModule
+{
+    class ETCLoadModule : IModule
+    {
+        IRegionManager regionManager;
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            regionManager = containerProvider.Resolve<IRegionManager>();
+            Manager.RegionCreated += Manager_RegionCreated;
+            Manager.RegionRequest();
+        }
+
+        private void Manager_RegionCreated(string regName)
+        {
+            //regionManager.RegisterViewWithRegion(regName, typeof(ViewB));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
