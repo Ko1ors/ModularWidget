d9f066a045e3b9dae2eddcc05e33b8081eddb1ae
Ko1ors <kolorsnik181200@gmail.com>
2020-10-02 23:38:12 +0300

    MusicWinModule package update

---
+++ MusicWinModule/Views/MusicUC.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Controls;
+using Windows.Media.Control;
+
+namespace MusicWinModule.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MusicUC.xaml
+    /// </summary>
+    public partial class MusicUC : UserControl
+    {
+        public MusicUC()
+        {
+            InitializeComponent();
+            var gsmtcsm = GlobalSystemMediaTransportControlsSessionManager.RequestAsync().GetAwaiter().GetResult().GetCurrentSession();
+            var mediaProperties = gsmtcsm.TryGetMediaPropertiesAsync().GetAwaiter().GetResult();
+            thumbnail = mediaProperties.Thumbnail;
+        }
+    }
+}
