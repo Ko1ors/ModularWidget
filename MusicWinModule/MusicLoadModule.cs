5f26499ed93c74f1ca0d9209c9782688b582c5f0
Ko1ors <kolorsnik181200@gmail.com>
2020-10-02 22:16:52 +0300

    MusicLoadModule update

---
+++ MusicWinModule/MusicLoadModule.cs
@@ -0,0 +1,38 @@
+﻿using ModularWidget;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Windows.Controls;
+
+namespace MusicWinModule
+{
+    public class MusicLoadModule : IModule
+    {
+        IRegionManager regionManager;
+        private readonly string regName = "musicregion";
+        private UserControl musicView;
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            regionManager = containerProvider.Resolve<IRegionManager>();
+            Manager.RegionCreated += Manager_RegionCreated;
+            Manager.RegionRequest(regName);
+        }
+
+
+        private void Manager_RegionCreated(string regName)
+        {
+            if (regName == this.regName)
+            {
+                Manager.RegionCreated -= Manager_RegionCreated;
+                //regionManager.RegisterViewWithRegion(regName, typeof(MainUC));
+                regionManager.Regions[regName].Add(musicView);
+            }
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
