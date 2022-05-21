40676484afa3faac2cf2057f96fffb34e3024915
Ko1ors <kolorsnik181200@gmail.com>
2022-05-21 19:49:43 +0300

    drag & drop improved, multiple widget windows now available

---
+++ ModularWidget/Services/IWindowService.cs
@@ -0,0 +1,28 @@
+﻿using ModularWidget.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModularWidget.Services
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Win32Point
+    {
+        public Int32 X;
+        public Int32 Y;
+    };
+
+    public interface IWindowService
+    {
+        void AddWindow(MainWindow window);
+
+        void RemoveAndCloseWindow(MainWindow window);
+
+        bool CursorInsideWindow(Win32Point w32Mouse);
+
+        MainWindow FindWindowByRegion(RegionUC region);
+    }
+}
