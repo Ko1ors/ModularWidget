8c6aa6288facad4e1d54c68470e212b0936eb3e5
Ko1ors <kolorsnik181200@gmail.com>
2022-07-03 13:44:34 +0300

    ETH/ETC modules refactoring: thread sleep replaced with task delay

--- ETCModule/Services/EtcService.cs
+++ ETCModule/Services/EtcService.cs
@@ -87,7 +87,7 @@ namespace ETCModule.Services
                     priceModel.Price.CoinUsd = Math.Round(priceModel.Price.CoinUsd ?? -1, 2);
                     return priceModel;
                 }
-                Thread.Sleep(500 * (i + 1));
+                await Task.Delay(500 * (i + 1));
             }
             return null;
         }
@@ -101,7 +101,7 @@ namespace ETCModule.Services
                 {
                     return Math.Round(float.Parse(walletBalanceModel.Result) / _divisor, 4);
                 }
-                Thread.Sleep(500 * (i + 1));
+                await Task.Delay(500 * (i + 1));
             }
             return -1;
         }
