c1a5edb66341c251dce250bd2f19dd6a02986f1a
Ko1ors <kolorsnik181200@gmail.com>
2022-07-04 22:28:01 +0300

    CMC Portfolio Module base setup, base userControl added

---
+++ CoinMarketCapPortfolioModule/CoinMarketCapPortfolioLoadModule.cs
@@ -0,0 +1,19 @@
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace CoinMarketCapPortfolioModule
+{
+    public class CoinMarketCapPortfolioLoadModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
