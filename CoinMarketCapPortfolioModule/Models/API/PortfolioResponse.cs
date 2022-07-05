c77deb05e2e1fa9830cd32ae41dbd6c244aaac2d
Ko1ors <kolorsnik181200@gmail.com>
2022-07-05 21:43:42 +0300

    CMC Portfolio Module API response models added

---
+++ CoinMarketCapPortfolioModule/Models/API/PortfolioResponse.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinMarketCapPortfolioModule.Models.API
+{
+    public class PortfolioResponse
+    {
+        [JsonProperty("data")]
+        public List<Portfolio> Portfolios { get; set; }
+
+        [JsonProperty("status")]
+        public Status Status { get; set; }
+    }
+}
